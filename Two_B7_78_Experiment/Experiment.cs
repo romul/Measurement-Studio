41996303180af997bf388ee5b7bcad17fe2bed8b
Roman Smirnov <POMAHC@gmail.com>
2009-08-06 21:32:58 +0400

    Add plugin to work with two B7 78/1 voltmeters.

---
+++ Two_B7_78_Experiment/Experiment.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Common;
+using TSU.Voltmeters;
+using System.Windows.Forms;
+
+namespace Two_B7_78_Experiment
+{
+    class Experiment : AbstractExperiment
+    {
+        private readonly VoltmeterControl B7_78_1, B7_78_2;
+        private int count;
+        private Point3D point;
+
+        private TSettings CustomSettings
+        {
+            get { return Settings as TSettings; }
+        }
+
+        public Experiment()
+        {
+            B7_78_1 = new VoltmeterControl();
+            B7_78_1.DataReceived += OnDataReceived;
+            B7_78_2 = new VoltmeterControl();
+            B7_78_2.DataReceived += OnDataReceived;
+            Settings = new TSettings();
+            ExperimentCaption = "Два B7 78/1";
+        }
+
+        private void OnDataReceived(object sender, DataReceivedEventArgs args)
+        {            
+            if (point.Z == null)
+            {
+                var y = CustomSettings.Y1Transform.Call(args.Value);
+                point = new Point3D(count * 1e-3 * Settings.MeasurementPeriod, y);
+            }
+            else
+            {
+                var z = CustomSettings.Y2Transform.Call(args.Value);
+                point.Z = z;
+                Chart.AddPoint(point);
+                Data.SavePoint(point);
+                count++;
+            }
+        }
+
+        #region Overrides of AbstractExperiment
+
+        protected override ConnectStatus TryConnect()
+        {
+            B7_78_1.Interval = CustomSettings.MeasurementPeriod;
+            B7_78_1.IsDoubleMeasure = false;
+            B7_78_1.DeviceName = String.Format("USB0::0x164E::0x0DAD::TW0000{0}::INSTR", CustomSettings.FirstDeviceId);
+            B7_78_2.Interval = CustomSettings.MeasurementPeriod;
+            B7_78_2.IsDoubleMeasure = false;
+            B7_78_2.DeviceName = String.Format("USB0::0x164E::0x0DAD::TW0000{0}::INSTR", CustomSettings.SecondDeviceId);
+            try
+            {
+                B7_78_1.Setup(CustomSettings.FirstInitCommands.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+                B7_78_2.Setup(CustomSettings.SecondInitCommands.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+            catch (VoltmeterException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+
+            if (B7_78_1.Connected && B7_78_2.Connected)
+            {
+                CurrentState = MeasurementState.Connected;
+                return ConnectStatus.Successful;
+            }
+            else if (B7_78_1.Connected || B7_78_2.Connected)
+            {
+                return ConnectStatus.OneDeviceNotConnected;
+            }
+            return ConnectStatus.AllDevicesNotConnected;
+        }
+
+        public override void StartMeasurement()
+        {
+            base.StartMeasurement();
+            B7_78_1.StartReading();
+            B7_78_2.StartReading();
+        }
+
+        public override void StopMeasurement()
+        {
+            B7_78_1.StopReading();
+            B7_78_2.StopReading();
+        }
+
+        public override void Disconnect()
+        {
+            B7_78_1.Disconnect();
+            B7_78_2.Disconnect();
+            if (!B7_78_1.Connected || !B7_78_2.Connected)
+            {
+                CurrentState = MeasurementState.Disconnected;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Two_B7_78_Experiment/TSettings.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+
+namespace Two_B7_78_Experiment
+{
+    public class TSettings : TwoDevicesSettings
+    {
+        [Text(LineCount = 3, MaxLength = 1000)]
+        [SetByUser(Caption = "Команды инициализации", Category = "1-ое устройство")]
+        public string FirstInitCommands { get; set; }
+
+        [Text(MaxLength = 4, Default = "4557")]
+        [SetByUser(Caption = "Id устройства", Category = "1-ое устройство")]
+        public string FirstDeviceId { get; set; }
+
+        [Text(LineCount = 3, MaxLength = 1000)]
+        [SetByUser(Caption = "Команды инициализации", Category = "2-ое устройство")]
+        public string SecondInitCommands { get; set; }
+
+        [Text(MaxLength = 4, Default = "4557")]
+        [SetByUser(Caption = "Id устройства", Category = "2-ое устройство")]
+        public string SecondDeviceId { get; set; }
+
+    }
+}
